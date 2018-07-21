5cfa054206c4f18efe19e3efb79e93d78663c774
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-07-21 10:31:15 -0300

    Adicionando interface IDAO para abstração do acesso ao banco de dados

---
+++ UsuarioAPI/DAO/IDAO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UsuarioAPI.DAO
+{
+    public interface IDAO<T>
+    {
+        void Cadastro(T model);
+
+        IList<T> Busca(T model);
+
+    }
+}
