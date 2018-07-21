f58feb5231668a328f4d8d616d4831da11b30544
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-07-21 10:32:31 -0300

    Adicionando classe UsuarioModel para representação da entidade Usuario

---
+++ UsuarioAPI/Models/UsuarioModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsuarioAPI.Models
+{
+    public class UsuarioModel
+    {
+        public int IdUsuario { get; set; }
+
+        public string Nome { get; set; }
+
+        public byte Idade { get; set; }
+
+        public DateTime DtNascimento { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
