203ae74bac2dd3a01659697585fd9044960f0295
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-07-30 21:59:34 -0300

    Adicionando testes para camada de acesso aos dados (DAO)

---
+++ UsuarioAPI.Test/DAO/UsuarioDAOTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UsuarioAPI.DAO;
+using UsuarioAPI.Models;
+using Xunit;
+
+namespace UsuarioAPI.Test.DAO
+{
+    public class UsuarioModelTest
+    {
+        [Fact]
+        public void TesteBuscaVazia()
+        {
+            var DAO = new UsuarioDAO("Password=xJi8j9WXU7m6u7ResP9SNNPW;Persist Security Info=True;User ID=API_USER;Initial Catalog=API;Data Source=LOCALHOST");
+            IList<UsuarioModel> resultado = DAO.Busca(new UsuarioModel());
+
+            Assert.True(true);
+        }
+    }
+}
